43066fece7c13121e8b0461f8ab1be271d94a7e4
Ross <ross.lillis@gmail.com>
2021-04-14 19:23:52 +1000

    feat: add timesheet validations

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,3 +1,5 @@
 class Timesheet < ApplicationRecord
-
+  validates :date, presence: { message: "Date is required"}
+  validates :start_time, presence: { message: "Start time is required"}
+  validates :finish_time, presence: { message: "Finish time is required"}
 end
