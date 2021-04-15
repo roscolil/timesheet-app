6391b25994c6c94c2a372753d339f08968bdc968
Ross <ross.lillis@gmail.com>
2021-04-15 20:01:11 +1000

    add gem for form validations and error messageing partials

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,5 +1,15 @@
 class Timesheet < ApplicationRecord
-  validates :date, presence: { message: "Date is required"}
-  validates :start_time, presence: { message: "Start time is required"}
-  validates :finish_time, presence: { message: "Finish time is required"}
+  validates_date :date,
+                  on_or_before: :today,
+                  message: "cannot be in the future",
+                  presence: true
+
+  validates :start_time,
+            presence: true
+
+  validates_time :finish_time,
+            after: :start_time,
+            message: "can't be at or before your start time",
+            presence: true
+
 end
